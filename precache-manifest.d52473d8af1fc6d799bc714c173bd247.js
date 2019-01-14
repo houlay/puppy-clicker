40672c03c4bfda1bd07b218d78df70f62278ceed
Chad Pan <cppan0@gmail.com>
2019-01-13 21:31:08 -0600

    Updates

---
+++ precache-manifest.d52473d8af1fc6d799bc714c173bd247.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "627b0588d2c06452b987",
+    "url": "/puppy-clicker/static/js/runtime~main.627b0588.js"
+  },
+  {
+    "revision": "c96cb7a6367872e815b6",
+    "url": "/puppy-clicker/static/js/main.c96cb7a6.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/puppy-clicker/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "c96cb7a6367872e815b6",
+    "url": "/puppy-clicker/static/css/main.c9f96211.chunk.css"
+  },
+  {
+    "revision": "9029dec9f77fa41f9845d34cc42f34c6",
+    "url": "/puppy-clicker/index.html"
+  }
+];
